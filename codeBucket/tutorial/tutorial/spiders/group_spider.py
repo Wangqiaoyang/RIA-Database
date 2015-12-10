01b9a6cc99618914777d1d3ae0776b004b337b9e
faketooth <faketooh@qq.com>
2015-12-11 00:33:11 +0800

    Changes to be committed: 	deleted:    tutorial/test.md 	copied:     tutorial/tutorial/spiders/test_spider.py -> tutorial/tutorial/spiders/group_spider.py 	modified:   tutorial/tutorial/spiders/test_spider.py
    获取小组内全部帖子链接
    每个帖子取楼主帖子内容写入topic目录下md文件中

--- codeBucket/tutorial/tutorial/spiders/group_spider.py
+++ codeBucket/tutorial/tutorial/spiders/group_spider.py
@@ -2,8 +2,8 @@
 import re
 import scrapy
 import BeautifulSoup
-from scrapy.contrib.spiders import CrawlSpider,Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.spiders import CrawlSpider,Rule
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 
 class GroupSpider(scrapy.Spider):
@@ -12,7 +12,8 @@ class GroupSpider(scrapy.Spider):
     start_urls=[
     "http://www.douban.com/group/562894/discussion"
     ]
-
+    
+    discussion = []
     
     def makelist(self, table):
     	result = []
@@ -21,7 +22,7 @@ class GroupSpider(scrapy.Spider):
     		result.append([])
     		allcols = row.findAll('td')
     		tmp = allcols[0].findAll('a')
-    		if tmp != []:
+    		if tmp:
     			href = tmp[0].get("href")
     			title = tmp[0].get("title")
     			result[-1].append(href)
@@ -30,23 +31,26 @@ class GroupSpider(scrapy.Spider):
     
     def parse(self, response):
         sel=Selector(response)
-        a = sel.xpath('//*[@id="content"]/div/div[1]/div[2]/table').extract()
-        
-        print
-        for i in a:
-        	html = i.encode("utf8")
+        tables = sel.xpath('//*[@id="content"]/div/div[1]/div[2]/table').extract()
+        for table in tables:
+        	html = table.encode("utf8")
         	soup = BeautifulSoup.BeautifulSoup(html)
         	for item in self.makelist(soup.findAll('table')[0]):
         		if item == []:
         			continue
-        		print item[0], item[1]
-        print
+        		self.discussion.append(item[0])
         
         nextlink = sel.xpath('//*[@id="content"]/div/div[1]/div[3]/span[3]/link').extract()
         if nextlink :
         	soup = BeautifulSoup.BeautifulSoup(nextlink[0])
         	next = soup.findAll('link')[0].get("href")
-        	yield scrapy.Request(response.urljoin(next),callback=self.parse ) 
+        	yield scrapy.Request(response.urljoin(next),callback=self.parse)
+        else:
+        	file = open('discussion_list.txt', 'wb')
+        	for url in self.discussion:
+        		file.write(url+"\n")
+        	file.close()
+        
         
     
     
--- codeBucket/tutorial/tutorial/spiders/test_spider.py
+++ codeBucket/tutorial/tutorial/spiders/test_spider.py
@@ -3,63 +3,30 @@ import re
 import html2text
 import scrapy
 import BeautifulSoup
-from scrapy.contrib.spiders import CrawlSpider,Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.spiders import CrawlSpider,Rule
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 
 class GroupSpider(scrapy.Spider):
     name = 'testspider'
     allowed_domains = ["douban.com"]
-    start_urls=[
-    #"http://www.douban.com/group/topic/81573808/",
-    #"http://www.douban.com/group/topic/81959306/",
-    "http://www.douban.com/group/topic/79588017/"
-    ]
+    file = open('discussion_list.txt', 'r')
+    start_urls = []
+    for line in file.readlines():
+    	start_urls.append(line.strip('\n'))
+    	
     
     def parse(self, response):
+    	filename = "topic/topic_"+response.url.split('/')[-2]
     	sel=Selector(response)
     	a = sel.xpath('//*[@id="link-report"]/div[1]').extract()[0]
-    	file = open('test.md', 'wb')
+    	
+    	file = open(filename+'.md', 'wb')
     	h = html2text.HTML2Text()
     	text =  h.handle(a)
     	file.write(text.encode("utf8"))
     	file.close()
     
-"""    
-    def makelist(self, table):
-    	result = []
-    	allrows = table.findAll('tr')
-    	for row in allrows:
-    		result.append([])
-    		allcols = row.findAll('td')
-    		tmp = allcols[0].findAll('a')
-    		if tmp != []:
-    			href = tmp[0].get("href")
-    			title = tmp[0].get("title")
-    			result[-1].append(href)
-    			result[-1].append(title)
-    	return result
-    
-    def parse(self, response):
-        sel=Selector(response)
-        a = sel.xpath('//*[@id="content"]/div/div[1]/div[2]/table').extract()
-        
-        print
-        for i in a:
-        	html = i.encode("utf8")
-        	soup = BeautifulSoup.BeautifulSoup(html)
-        	for item in self.makelist(soup.findAll('table')[0]):
-        		if item == []:
-        			continue
-        		print item[0], item[1]
-        print
-        
-        nextlink = sel.xpath('//*[@id="content"]/div/div[1]/div[3]/span[3]/link').extract()
-        if nextlink :
-        	soup = BeautifulSoup.BeautifulSoup(nextlink[0])
-        	next = soup.findAll('link')[0].get("href")
-        	yield scrapy.Request(response.urljoin(next),callback=self.parse ) 
-"""   
     
     
         
