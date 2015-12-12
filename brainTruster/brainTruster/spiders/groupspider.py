4406735f21a2cbdd26b7a4920b05cf12ec5c4933
faketooth <faketooh@qq.com>
2015-12-12 21:36:34 +0800

    start to refactor topic spider

--- brainTruster/brainTruster/spiders/groupspider.py
+++ brainTruster/brainTruster/spiders/groupspider.py
@@ -1,5 +1,7 @@
 # -*- coding: utf
+import os,sys
 import scrapy
+import html2text
 from scrapy.spiders import CrawlSpider,Rule
 from scrapy.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
@@ -14,9 +16,6 @@ class GroupSpider(scrapy.Spider):
     discussion_list = []
     
     topic_path = 'topic'
-    
-    def parse_discussion(self, response):
-    	pass
     
     def parse(self, response):
     	sel=Selector(response)
@@ -35,7 +34,53 @@ class GroupSpider(scrapy.Spider):
         	yield scrapy.Request(response.urljoin(link),callback=self.parse)
         else:
         	# all discussion topics have been crawled. start to process every topic.
+        	# make sure the path to store topic markdown file exist.
+        	if not os.path.exists(self.topic_path):
+        		msg = "\n\nERROR: Directory %s doesn't exit. Please check it again.\n" 
+        		sys.exit(msg % self.topic_path)
+        		
         	for discussion in self.discussion_list:
-        		yield scrapy.Request(response.urljoin(discussion),callback=self.parse_discussion)  	
+        		yield scrapy.Request(response.urljoin(discussion),callback=self.parse_discussion)
     
+    def parse_discussion(self, response):
+    	filename = self.topic_path + "/topic_"+response.url.split('/')[-2]
+    	sel=Selector(response)
+    	
+    	title = self.handle_title(sel.xpath('//*[@id="content"]/h1/text()').extract())
+    	from_user = self.handle_fromuser(sel)
+    	
+    	
+    	reply_docs = self.handle_comments(sel.xpath('//*[@class="reply-doc content"]'))
+    	a = sel.xpath('//*[@id="link-report"]/div[1]').extract()[0]
+    	file = open(filename+'.md', 'wb')
+    	h = html2text.HTML2Text()
+    	text =  h.handle(a)
+    	file.write(title.encode("utf8") + "\n---\n")
+    	file.write(from_user.encode("utf8"))
+    	file.write(text.encode("utf8"))
+    	for reply in reply_docs:
+    		file.write(reply.encode("utf8"))
+    	file.close()
+    
+    def handle_fromuser(self, sel):
+    	from_user = sel.xpath('//*[@class="topic-doc"]/h3/span[1]/a/text()').extract()[0]
+    	from_user_href = sel.xpath('//*[@class="topic-doc"]/h3/span[1]/a/@href').extract()[0]
+    	pubtime = sel.xpath('//*[@class="topic-doc"]/h3/span[2]/text()').extract()[0]
+    	result =  "###" + "[" + from_user + "]" + "(" + from_user_href + ")" + "\t" + pubtime + "\n"
+    	return result
+    
+    def handle_title(self, list):
+    	headline = list[0].strip('\n').lstrip(' ')
+    	return "#" + headline
+    
+    def handle_comments(self, comments):
+    	replys = []
+    	for comment in comments:
+    		herf = comment.xpath('.//div[1]/h4/a/@href').extract()[0]
+    		username = comment.xpath('.//div[1]/h4/a/text()').extract()[0]
+    		pubtime = comment.xpath('.//div[1]/h4/span/text()').extract()[0]
+    		reply = html2text.HTML2Text().handle(comment.xpath('.//p').extract()[0])
+    		title = "---\n###" + "[" + username + "]" + "(" + herf + ")" + "\t" + pubtime + "\n"
+    		replys.append(title + reply)
+    	return replys
         
